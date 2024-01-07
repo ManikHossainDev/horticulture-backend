f6f8a39f3189224a008d10593cb21109bff496cf
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-08 00:08:47 +0600

    form dynamik

--- src/page/UseStateFrom.tsx
+++ src/page/UseStateFrom.tsx
@@ -2,14 +2,20 @@
 import { useState } from "react";
 const UseStateFrom = () => {
      const [use, setUse] = useState({name:'', email:''})
-     const handelForm = (e:React.ChangeEvent<HTMLFormElement>) => {
+     const handelFormSubmit = (e:React.ChangeEvent<HTMLFormElement>) => {
             e.preventDefault()
             console.log(use)
      }
+    const handelInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        const inputName = e.target.name;
+        const value = e.target.value;
+        setUse({...use, [inputName]:value});
+    }
+
     return (
-        <form onSubmit={handelForm}>
-            <input onChange={(e)=> setUse({...use,email:e.target.value})} className="border border-red-300" type="text" name="email" id="email" />
-            <input onChange={(e)=> setUse({...use, name:e.target.value})} className="border border-red-300" type="text" name="name" id="name" />
+        <form onSubmit={handelFormSubmit}>
+            <input onChange={handelInputChange} className="border border-red-300" type="text" name="email" id="email" />
+            <input onChange={handelInputChange} className="border border-red-300" type="text" name="name" id="name" />
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">submit</button>
         </form>
     );
