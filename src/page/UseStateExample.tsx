dd5fd4c09ab7ff80e1cfe332557752d928fd50ab
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-07 22:42:07 +0600

    add function

--- src/page/UseStateExample.tsx
+++ src/page/UseStateExample.tsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
 
 const UseStateExample = () => {
-    const [counter, setCounter] = useState(0)
-    return (
-        <div>
-            <h1>{counter}</h1>
-        </div>
-    );
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setCounter(counter + 1)}
+      >
+        count
+      </button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCounter(counter - 1)}>
+        docent
+      </button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCounter(0)}>
+        rest
+      </button>
+    </div>
+  );
 };
 
 export default UseStateExample;
