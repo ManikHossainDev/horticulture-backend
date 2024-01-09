24b9fbc88f27864294ee601abb92f716621f47e5
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-10 01:58:15 +0600

    context provider add project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 // import { useState } from "react";
+import { useContext } from "react";
 import "./App.css";
 // import UseStateExample from "./page/UseStateExample";
 // import UseStateFrom from "./page/UseStateFrom";
@@ -7,18 +8,25 @@ import "./App.css";
 // import UseEffectExample from "./page/UseEffect";
 // import UserFormReducer from "./page/UserFormReducer";
 import UseRef from "./page/UseRef";
+import { contextProvider } from "./context/ThemeProvider";
 
 function App() {
   // const [counter, setCounter] = useState(0);
 
+  const {dark, setDark} = useContext(contextProvider)
+
   return (
-    <>
+    < >
       {/* <UseStateExample counter={counter} setCounter={setCounter} />
       <UseStateFrom />
       <UseReducer />
       <UserFormReducer />
       <UseEffectExample /> */}
-      <UseRef />
+      {/* <UseRef /> */}
+      <div className={`h-screen w-full flex justify-center items-center ${dark ? "bg-black":"bg-white"}`}>
+      <button className="bg-blue-500" onClick={() => setDark(!dark)}>Toggle</button>
+
+      </div>
     </>
   );
 }
