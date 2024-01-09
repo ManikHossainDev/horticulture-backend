0de9981a8eb933da682be332650491e20a2bb130
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-01-09 22:02:30 +0600

    project form usereducer use

--- src/page/userFormReducer.tsx
+++ src/page/userFormReducer.tsx
@@ -1,15 +1,19 @@
-import { useReducer } from "react";
-
-
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useReducer } from "react";
 
 const initialState = {name:'', email:''}
 
-const reducer = (currentState, action) => {
+type Taction = {
+    type:string;
+    payload:string;
+}
+
+const reducer = (currentState : typeof initialState, action:Taction) => {
  switch(action.type){
-    case 'increment':
-        return{count: currentState.count + 1 };
-    case 'decrement':
-        return{ count: currentState.count - 1 };
+    case 'addName':
+        return{...currentState, name: action.payload };
+    case 'addEmail':
+        return{...currentState, email: action.payload };
     default :
         return currentState ;
  }
@@ -19,13 +23,18 @@ const reducer = (currentState, action) => {
 const userFormReducer = () => {
     
     const [state, dispatch] = useReducer(reducer, initialState)
+
+    const handleSubmit = (e:React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(state)
+    }
+        
     return (
-        <div>
-            <h1>{state.count}</h1>
-            <button onClick={() => dispatch({type:'increment'})} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">increment</button>
-            <button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">rest</button>
-            <button onClick={() => dispatch({type:'decrement'})}  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">decrement</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input onChange={(e) => dispatch({type:'addEmail', payload:e.target.value})} className="border border-red-300" type="text" name="email" id="email" />
+            <input onChange={(e) => dispatch({type:'addName', payload:e.target.value})} className="border border-red-300" type="text" name="name" id="name" />
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">submit</button>
+        </form>
     );
 };
 
